1f9297a974f52d8f40a1adf1447f5808b37ffd58
indrajithac <indrajith@eguidancelabs.com>
2024-09-01 15:59:01 +0530

    routing structure

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <div>
         <h1>Home Page</h1>
+        <Link href="/about">About Page</Link>
       </div>
     </main>
   );
